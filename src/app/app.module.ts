c76fce957995cf89f2810d7ebe2f553aada02cc9
Parthiban <parthiban.g4@cognizant.com>
2018-04-23 19:52:47 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { Camera } from '@ionic-native/camera';
+import { Network } from '@ionic-native/network';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    InAppBrowser,
+    Camera,
+    Network,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { NavController, ToastController} from 'ionic-angular';
+
+import { Subscription } from 'rxjs/Subscription';
+
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { Network } from '@ionic-native/network';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController,
+              public toastCtrl: ToastController,
+              public iab: InAppBrowser,
+              public network: Network) {
+
+  }
+
+  connected: Subscription;
+  disconnected: Subscription;
+
+  ionViewCanEnter(){
+    this.connected= this.network.onConnect().subscribe(data=>{
+      this.networkUpdate(data.type);
+    },error=>{
+ 
+    });
+ 
+    this.disconnected=this.network.onDisconnect().subscribe(data=>{
+     this.networkUpdate(data.type);
+    },error=>{
+ 
+    });
+  }
+
+  networkUpdate(connectionState: string){
+    this.toastCtrl.create({
+      message:'You are now '+connectionState+' via '+this.network.type,
+      duration:3000
+    }).present();
+  }
+
+  ionViewDidLoad(){
+    const browser = this.iab.create('http://argus.workiy.org','_self',{location:'no'}); 
+  }
+
+  
+
+}
